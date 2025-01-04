8941369a8ccc076fde1db4665bd7f61dc4d2788c
Polina Muravieva <polinamur3@gmail.com>
2025-01-04 19:54:47 +0300

    feat: implement user logout functionality

--- frontend/src/app/core/auth/auth.service.ts
+++ frontend/src/app/core/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {Observable, Subject} from "rxjs";
+import {Observable, Subject, throwError} from "rxjs";
 import {DefaultResponseType} from "../../../types/default-response.type";
 import {LoginResponseType} from "../../../types/login-response.type";
 import {HttpClient} from "@angular/common/http";
@@ -24,6 +24,16 @@ export class AuthService {
     return this.http.post<DefaultResponseType | LoginResponseType>(environment.api + 'login', {
       email, password, rememberMe
     })
+  }
+
+  logout(): Observable<DefaultResponseType> {
+    const tokens = this.getTokens();
+    if (tokens && tokens.refreshToken) {
+      return this.http.post<DefaultResponseType>(environment.api + 'logout', {
+        refreshToken: tokens.refreshToken
+      })
+    }
+    throw throwError(() => 'Can not find token');
   }
 
   public getIsLoggedIn() {
--- frontend/src/app/shared/layout/header/header.component.ts
+++ frontend/src/app/shared/layout/header/header.component.ts
@@ -1,6 +1,10 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {CategoryType} from "../../../../types/category.type";
 import {AuthService} from "../../../core/auth/auth.service";
+import {DefaultResponseType} from "../../../../types/default-response.type";
+import {HttpErrorResponse} from "@angular/common/http";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -12,7 +16,9 @@ export class HeaderComponent implements OnInit {
   isLogged: boolean = false;
   @Input() categories: CategoryType[] = [];
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService,
+              private _snackBar: MatSnackBar,
+              private router: Router) {
     this.isLogged = this.authService.getIsLoggedIn();
   }
 
@@ -23,6 +29,21 @@ export class HeaderComponent implements OnInit {
   }
 
   logout(): void {
+    this.authService.logout()
+      .subscribe({
+        next: () => {
+          this.doLogout();
+        },
+        error: () => {
+         this.doLogout();
+        }
+      })
+  }
 
+  doLogout(): void {
+    this.authService.removeTokens();
+    this.authService.userId = null;
+    this._snackBar.open('Вы вышли из системы');
+    this.router.navigate(['/']);
   }
 }
