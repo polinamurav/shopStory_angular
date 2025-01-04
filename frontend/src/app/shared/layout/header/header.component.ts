2573d1168d8252cc5b2a02fe2b5dbfb7b16d6e37
Polina Muravieva <polinamur3@gmail.com>
2025-01-04 19:39:45 +0300

    feat: implement user authentication

--- frontend/src/app/shared/layout/header/header.component.ts
+++ frontend/src/app/shared/layout/header/header.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {CategoryType} from "../../../../types/category.type";
+import {AuthService} from "../../../core/auth/auth.service";
 
 @Component({
   selector: 'app-header',
@@ -8,10 +9,20 @@ import {CategoryType} from "../../../../types/category.type";
 })
 export class HeaderComponent implements OnInit {
 
+  isLogged: boolean = false;
   @Input() categories: CategoryType[] = [];
 
-  constructor() { }
+  constructor(private authService: AuthService) {
+    this.isLogged = this.authService.getIsLoggedIn();
+  }
 
   ngOnInit(): void {
+    this.authService.isLogged$.subscribe((isLoggedIn: boolean) => {
+      this.isLogged = isLoggedIn;
+    });
+  }
+
+  logout(): void {
+
   }
 }
