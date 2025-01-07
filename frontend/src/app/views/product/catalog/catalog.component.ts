127b6d8ccbe3134cf941684b0a4700d652240fbc
Polina Muravieva <polinamur3@gmail.com>
2025-01-08 01:01:53 +0300

    feat: fetch and display category filter text during product filtering

--- frontend/src/app/views/product/catalog/catalog.component.ts
+++ frontend/src/app/views/product/catalog/catalog.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ProductService} from "../../../shared/services/product.service";
 import {ProductType} from "../../../../types/product.type";
+import {CategoryService} from "../../../shared/services/category.service";
+import {CategoryWithTypeType} from "../../../../types/category-with-type.type";
 
 @Component({
   selector: 'app-catalog',
@@ -10,13 +12,20 @@ import {ProductType} from "../../../../types/product.type";
 export class CatalogComponent implements OnInit {
 
   products: ProductType[] = [];
+  categoriesWithTypes: CategoryWithTypeType[] = [];
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+              private categoryService: CategoryService) { }
 
   ngOnInit(): void {
     this.productService.getProducts()
       .subscribe(data => {
         this.products = data.items;
+      });
+
+    this.categoryService.getCategoriesWithTypes()
+      .subscribe(data => {
+        this.categoriesWithTypes = data;
       });
   }
 
