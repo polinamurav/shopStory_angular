4c7909d48d10770eb4eec85a28243bc121956a9f
Polina Muravieva <polinamur3@gmail.com>
2025-01-09 16:23:47 +0300

    feat: create category-filter component and update project structure

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -4,11 +4,13 @@ import {PasswordRepeatDirective} from "./directives/password-repeat.directive";
 import {ProductCardComponent} from './components/product-card/product-card.component';
 import {RouterModule} from "@angular/router";
 import {FormsModule} from "@angular/forms";
+import { CategoryFilterComponent } from './components/category-filter/category-filter.component';
 
 @NgModule({
   declarations: [
     PasswordRepeatDirective,
-    ProductCardComponent
+    ProductCardComponent,
+    CategoryFilterComponent
   ],
   imports: [
     CommonModule,
@@ -17,7 +19,8 @@ import {FormsModule} from "@angular/forms";
   ],
   exports: [
     PasswordRepeatDirective,
-    ProductCardComponent
+    ProductCardComponent,
+    CategoryFilterComponent
   ]
 })
 export class SharedModule {
