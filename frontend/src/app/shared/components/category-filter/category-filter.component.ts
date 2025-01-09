496ee7f77b35327269973e8ff61ebb8c5259a2b3
Polina Muravieva <polinamur3@gmail.com>
2025-01-09 16:55:14 +0300

    feat: add queryParams for type filtering

--- frontend/src/app/shared/components/category-filter/category-filter.component.ts
+++ frontend/src/app/shared/components/category-filter/category-filter.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {CategoryWithTypeType} from "../../../../types/category-with-type.type";
+import {Router} from "@angular/router";
+import {ActiveParamsType} from "../../../../types/active-params.type";
 
 @Component({
   selector: 'category-filter',
@@ -10,6 +12,8 @@ export class CategoryFilterComponent implements OnInit {
 
   @Input() categoryWithTypes: CategoryWithTypeType | null = null;
   @Input() type: string | null = null;
+  open = false;
+  activeParams: ActiveParamsType = {types: []};
 
   get title(): string {
     if (this.categoryWithTypes) {
@@ -25,9 +29,30 @@ export class CategoryFilterComponent implements OnInit {
     return '';
   }
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  toggle(): void {
+    this.open = !this.open;
+  }
+
+  updateFilterParam(url: string, checked: boolean) {
+    if (this.activeParams.types && this.activeParams.types.length > 0) {
+      const existingTypeInParams = this.activeParams.types.find(item => item === url);
+      if (existingTypeInParams && !checked) {
+        this.activeParams.types = this.activeParams.types.filter(item => item != url);
+      } else if (!existingTypeInParams && checked) {
+        this.activeParams.types.push(url);
+      }
+    } else if (checked) {
+      this.activeParams.types = [url];
+    }
+
+    this.router.navigate(['/catalog'], {
+      queryParams: this.activeParams
+    });
   }
 
 }
