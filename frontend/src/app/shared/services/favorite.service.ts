041601185b08a895db07d46ba935ac804ed9149f
Polina Muravieva <polinamur3@gmail.com>
2025-01-13 18:27:44 +0300

    fetch and display data on the favorite page

---
+++ frontend/src/app/shared/services/favorite.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {Observable} from "rxjs";
+import {ProductType} from "../../../types/product.type";
+import {environment} from "../../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {FavoriteType} from "../../../types/favorite.type";
+import {DefaultResponseType} from "../../../types/default-response.type";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FavoriteService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getFavorite(): Observable<FavoriteType[] | DefaultResponseType> {
+    return this.http.get<FavoriteType[] | DefaultResponseType>(environment.api + 'favorite');
+  }
+}
--- frontend/src/app/views/personal/favorite/favorite.component.ts
+++ frontend/src/app/views/personal/favorite/favorite.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FavoriteService} from "../../../shared/services/favorite.service";
+import {FavoriteType} from "../../../../types/favorite.type";
+import {DefaultResponseType} from "../../../../types/default-response.type";
+import {environment} from "../../../../environments/environment";
 
 @Component({
   selector: 'app-favorite',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoriteComponent implements OnInit {
 
-  constructor() { }
+  products: FavoriteType[] = [];
+  serverStaticPath = environment.serverStaticPath;
+
+  constructor(private favoriteService: FavoriteService) {
+  }
 
   ngOnInit(): void {
+    this.favoriteService.getFavorite()
+      .subscribe((data: FavoriteType[] | DefaultResponseType) => {
+        if ((data as DefaultResponseType).error !== undefined) {
+          const error = (data as DefaultResponseType).message;
+          throw new Error(error);
+        }
+
+        this.products = data as FavoriteType[];
+      })
+  }
+
+  removeFromFavorites(id: string) {
+
   }
 
 }
