3700e1a9c93e3ed36c664e41e4c95b0b3ccfd7f3
Polina Muravieva <polinamur3@gmail.com>
2025-01-15 17:50:00 +0300

    feat: add total price and item count calculation on order page

--- frontend/src/app/views/order/order/order.component.ts
+++ frontend/src/app/views/order/order/order.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {CartType} from "../../../../types/cart.type";
+import {DefaultResponseType} from "../../../../types/default-response.type";
+import {CartService} from "../../../shared/services/cart.service";
+import {Router} from "@angular/router";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-order',
@@ -7,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
-  constructor() { }
+  cart: CartType | null = null;
+  totalAmount: number = 0;
+  totalCount: number = 0;
+
+  constructor(private cartService: CartService,
+              private _snackBar: MatSnackBar,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.cartService.getCart()
+      .subscribe((data: CartType | DefaultResponseType) => {
+        if ((data as DefaultResponseType).error !== undefined) {
+          throw new Error((data as DefaultResponseType).message);
+        }
+
+        this.cart = data as CartType;
+        if (!this.cart || (this.cart && this.cart.items.length === 0)) {
+          this._snackBar.open('Корзина пустая');
+          this.router.navigate(['/']);
+          return;
+        }
+        this.calculateTotal();
+      });
+  }
+
+  calculateTotal() {
+    this.totalAmount = 0;
+    this.totalCount = 0;
+    if (this.cart) {
+      this.cart.items.forEach(item => {
+        this.totalAmount += item.quantity * item.product.price;
+        this.totalCount += item.quantity;
+      });
+    }
   }
 
 }
