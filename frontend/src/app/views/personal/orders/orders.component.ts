a12658d0eed13a0e1acec8ddfe6d471fc7cd012e
Polina Muravieva <polinamur3@gmail.com>
2025-01-18 00:26:38 +0300

    feat: add layout for the orders page

--- frontend/src/app/views/personal/orders/orders.component.ts
+++ frontend/src/app/views/personal/orders/orders.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {OrderService} from "../../../shared/services/order.service";
+import {OrderType} from "../../../../types/order.type";
+import {DefaultResponseType} from "../../../../types/default-response.type";
 
 @Component({
   selector: 'app-orders',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersComponent implements OnInit {
 
-  constructor() { }
+  orders: OrderType[] = [];
+
+  constructor(private orderService: OrderService) { }
 
   ngOnInit(): void {
+
+    this.orderService.getOrders()
+      .subscribe((data: OrderType[] | DefaultResponseType) => {
+        if ((data as DefaultResponseType).error !== undefined) {
+          throw new Error((data as DefaultResponseType).message);
+        }
+
+        this.orders = data as OrderType[];
+      });
   }
 
 }
--- frontend/src/types/order.type.ts
+++ frontend/src/types/order.type.ts
@@ -1,5 +1,6 @@
 import {DeliveryType} from "./delivery.type";
 import {PaymentType} from "./payment.type";
+import {OrderStatusType} from "./order-status.type";
 
 export type OrderType = {
   deliveryType: DeliveryType,
@@ -14,12 +15,13 @@ export type OrderType = {
   entrance?: string,
   apartment?: string,
   comment?: string
-  items?:
-    {
-      id: string,
-      // name: string,
-      quantity: number,
-      price: number,
-      total: number
-    }[]
+  items?: {
+    id: string,
+    name: string,
+    quantity: number,
+    price: number,
+    total: number
+  }[],
+  totalAmount?: number,
+  status?: OrderStatusType
 }
