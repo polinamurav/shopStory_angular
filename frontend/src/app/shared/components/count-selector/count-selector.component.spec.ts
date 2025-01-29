db410a607c51f0475a17bbc926ee39ba9aba22c2
Polina Muravieva <polinamur3@gmail.com>
2025-01-30 02:30:25 +0300

    test: add unit test 'should change value in input after decreasing' for count-selector component

--- frontend/src/app/shared/components/count-selector/count-selector.component.spec.ts
+++ frontend/src/app/shared/components/count-selector/count-selector.component.spec.ts
@@ -1,11 +1,19 @@
 import {CountSelectorComponent} from "./count-selector.component";
+import {ComponentFixture, TestBed} from "@angular/core/testing";
+import {FormsModule} from "@angular/forms";
 
 describe('count-selector', () => {
 
   let countSelectorComponent: CountSelectorComponent;
+  let fixture: ComponentFixture<CountSelectorComponent>
 
   beforeEach(() => {
-    countSelectorComponent = new CountSelectorComponent();
+    TestBed.configureTestingModule({
+      imports: [FormsModule],
+      declarations: [CountSelectorComponent]
+    });
+    fixture = TestBed.createComponent(CountSelectorComponent);
+    countSelectorComponent = fixture.componentInstance;
   });
 
   it('should have count set', () => {
@@ -44,5 +52,20 @@ describe('count-selector', () => {
     });
 
     countSelectorComponent.decreaseCount();
+  });
+
+  it('should change value in input after decreasing', (done: DoneFn) => {
+
+    countSelectorComponent.count = 5;
+    countSelectorComponent.decreaseCount();
+
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      const componentElement: HTMLElement = fixture.nativeElement;
+      const input: HTMLInputElement = componentElement.querySelector('input') as HTMLInputElement;
+
+      expect(input.value).toBe('4');
+      done();
+    })
   });
 })
